534bb49362ac1f75593c39ce6310a6c85927eca7
kacimi <k2acim31@gmail.com>
2021-01-21 12:50:49 +0100

    affichage des produits dans home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $product = $productRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'All products',
+            'products' => $product,
         ]);
     }
 }
